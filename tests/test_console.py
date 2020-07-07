14b1a304c535a9e2fe7374331d1129f7d2f118ef
Hub Plug <31110528+hubplug@users.noreply.github.com>
2020-07-07 22:54:37 +0800

    tests layout

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+# tests/test_console.py
+import click.testing
+
+from modern_python_setup import console
+
+
+def test_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+    
