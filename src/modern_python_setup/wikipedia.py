c5c193c08aac6f1f6ffbf681c7f47b65356306be
Hub Plug <31110528+hubplug@users.noreply.github.com>
2020-07-11 09:23:05 +0800

    wikipedia.py, test_console.py: chg'd to handle requests exception gracefully & chg'd test to test

--- src/modern_python_setup/wikipedia.py
+++ src/modern_python_setup/wikipedia.py
@@ -1,3 +1,4 @@
+import click
 import requests
 
 
@@ -5,6 +6,12 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        # handle requests exception gracefully, raise as click exception
+        message = str(error)
+        raise click.ClickException(message)
+
