80cffa211496bb70c8c1ef42683aaa8038f0da70
Hub Plug <31110528+hubplug@users.noreply.github.com>
2020-07-12 15:51:36 +0800

    noxfile.py: added docstrings & re-ordered sessions; .flake8: enabled docstring checking for noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+"""Nox sessions."""
+
 import tempfile
 from typing import Any
 
@@ -5,11 +7,32 @@ import nox
 from nox.sessions import Session
 
 
+# by default, Nox runs all sessions defined in noxfile.py.
+# use the --session (-s) option to restrict it to a specific session.
+# e.g. nox -rs tests (only run tests)
+# e.g. nox -rs lint (only run lint)
+
+# to list all sessions:
+# e.g. nox ----list-sessions
+
+
+# manually define sessions run by default, excluding "black" & "typeguard"
+# i.e. do not run black and modify file(s) all the time
+nox.options.sessions = "lint", "mypy", "pytype", "safety", "tests"
+
+# define package name
+package = "modern_python_setup"
+
+# define locations for linting & formatting
+locations = "src", "tests", "noxfile.py"
+
+
 # instead of simply using "poetry install" (like for pytest), which installs a whole
 # bunch of dependencies required for testing but not necessarily for linting &
 # formatting, use poetry to generate "dev only" "requirements.txt" and pass it to
 # pip to install only dev dependencies and with correct version (pinning)
 def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    """Install packages constrained by Poetry's lock file."""
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -24,47 +47,21 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-# nox session for automated tests
-@nox.session(python=["3.8", "3.7"])
-def tests(session: Session) -> None:
-    # no end-to-end tests for default automated unit testing
-    # e.g. nox -r
-    # use -m e2e to run end-to-end tests
-    # e.g. nox -rs tests-3.8 -- -m e2e
-    # (run end-to-end tests inside the testing environment for Python 3.8)
-    args = session.posargs or ["--cov", "-m", "not e2e"]
-    # session.run("poetry", "install", external=True)
-    # instead of poetry install everything, which includes all dev dependencies,
-    # use poetry install with --no-dev, and then install dev dependencies
-    # only necessary for testing, using install_with_constraints()
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
-    )
-    session.run("pytest", *args)
-
-
-# define package name
-package = "modern_python_setup"
-
-
-# nox session to do runtime type checking (typeguard)
-@nox.session(python=["3.8", "3.7"])
-def typeguard(session: Session) -> None:
-    args = session.posargs or ["-m", "not e2e"]
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
-    # run typeguard with pytest plugin by passing "--typeguard-packages=..."
-    session.run("pytest", f"--typeguard-packages={package}", *args)
-
-
-# locations for linting & formatting
-locations = "src", "tests", "noxfile.py"
+# nox session for code formatting (black)
+@nox.session(python="3.8")
+def black(session: Session) -> None:
+    """Run black code formatter."""
+    args = session.posargs or locations
+    # session.install("black")
+    # change do call install_with_constraints() instead of session.install()
+    install_with_constraints(session, "black")
+    session.run("black", *args)
 
 
 # nox session for linting
 @nox.session(python=["3.8", "3.7"])
 def lint(session: Session) -> None:
+    """Lint using flake8."""
     args = session.posargs or locations
     # session.install() installs package(s) into virtual env via pip
     # session.install("flake8")
@@ -118,27 +115,12 @@ def lint(session: Session) -> None:
     session.run("flake8", *args)
 
 
-# by default, Nox runs all sessions defined in noxfile.py.
-# use the --session (-s) option to restrict it to a specific session.
-# e.g. nox -rs tests (only run tests)
-# e.g. nox -rs lint (only run lint)
-
-
-# nox session for code formatting (black)
-@nox.session(python="3.8")
-def black(session: Session) -> None:
-    args = session.posargs or locations
-    # session.install("black")
-    # change do call install_with_constraints() instead of session.install()
-    install_with_constraints(session, "black")
-    session.run("black", *args)
-
-
 # nox session for dependency (library/package) security check (safety)
 # uses the poetry export command to convert poetry’s lock file to a requirements file
 # the standard tempfile module is used to create a temporary file for the requirements
 @nox.session(python="3.8")
 def safety(session: Session) -> None:
+    """Scan dependencies for insecure packages."""
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -158,6 +140,7 @@ def safety(session: Session) -> None:
 # nox session for type check (mypy)
 @nox.session(python=["3.8", "3.7"])
 def mypy(session: Session) -> None:
+    """Type-check using mypy."""
     args = session.posargs or locations
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
@@ -166,13 +149,40 @@ def mypy(session: Session) -> None:
 # nox session for type check (pytype)
 @nox.session(python="3.7")
 def pytype(session: Session) -> None:
-    """Run the static type checker."""
+    """Type-check using pytype."""
     # disable warnings for libraries / packages without type definitions
     args = session.posargs or ["--disable=import-error", *locations]
     install_with_constraints(session, "pytype")
     session.run("pytype", *args)
 
 
-# define sessions run by default, excluding "black"
-# i.e. do not run black and modify file(s) all the time
-nox.options.sessions = "lint", "mypy", "pytype", "safety", "tests"
+# nox session for automated tests
+@nox.session(python=["3.8", "3.7"])
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    # no end-to-end tests for default automated unit testing
+    # e.g. nox -r
+    # use -m e2e to run end-to-end tests
+    # e.g. nox -rs tests-3.8 -- -m e2e
+    # (run end-to-end tests inside the testing environment for Python 3.8)
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    # session.run("poetry", "install", external=True)
+    # instead of poetry install everything, which includes all dev dependencies,
+    # use poetry install with --no-dev, and then install dev dependencies
+    # only necessary for testing, using install_with_constraints()
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
+
+
+# nox session to do runtime type checking (typeguard)
+@nox.session(python=["3.8", "3.7"])
+def typeguard(session: Session) -> None:
+    """Runtime type checking using Typeguard."""
+    args = session.posargs or ["-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
+    # run typeguard with pytest plugin by passing "--typeguard-packages=..."
+    session.run("pytest", f"--typeguard-packages={package}", *args)
